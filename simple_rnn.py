51e35fba6cd1e41670f64983c6f92322457440ac
satojkovic <satojkovic@gmail.com>
2017-02-23 23:31:28 +0900

    Add new script

---
+++ simple_rnn.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+RNN basics:
+    S_{k} = f( S_{k-1} * W_{rec} + X_{k} * W_{x} )
+"""
+
+import numpy as np
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
