ae99daa90151ea164cdbfa6591f84a26628e8705
satojkovic <satojkovic@gmail.com>
2017-02-25 01:30:28 +0900

    Add methods used in forward steps

--- simple_rnn.py
+++ simple_rnn.py
@@ -7,6 +7,26 @@ RNN basics:
 
 import numpy as np
 
+# Input parameters
+n_samples = 20
+seq_len = 10
+
+
+def update_state(xk, sk, wx, wRec):
+    return sk * wRec + xk * wx
+
+
+def forward_states(X, wx, wRec):
+    S = np.zeros((X.shape[0], X.shape[1] + 1))
+    for k in range(X.shape[1]):
+        S[:, k + 1] = update_state(X[:, k], S[:, k - 1], wx, wRec)
+    return S
+
+
+def cost(y, t):
+    return ((t - y)**2).sum() / n_samples
+
+
 
 def main():
     pass
